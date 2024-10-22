6afb8e422aba694b83cb5803653a8a941d968b97
Orician <2018783812@qq.com>
2024-10-22 11:21:11 +0800

    fix condition bugs

--- internal/code/code.go
+++ internal/code/code.go
@@ -4,15 +4,15 @@ import "oj-micro/common/xcode"
 
 var (
 	// InvalidParams api参数错误
-	InvalidParams = xcode.New(03201, "api参数错误")
+	InvalidParams = xcode.New(3201, "api参数错误")
 	// UnauthorizedTokenTimeout jwt授权过期
-	UnauthorizedTokenTimeout = xcode.New(03202, "token已过期")
+	UnauthorizedTokenTimeout = xcode.New(3202, "token已过期")
 	// UnauthorizedUserNotLogin jwt未授权
-	UnauthorizedUserNotLogin = xcode.New(03203, "用户未登录")
+	UnauthorizedUserNotLogin = xcode.New(3203, "用户未登录")
 	// UnauthorizedUserNotSuperUser 登录用户不是超级用户
-	UnauthorizedUserNotSuperUser = xcode.New(03204, "登录用户不是超级用户")
+	UnauthorizedUserNotSuperUser = xcode.New(3204, "登录用户不是超级用户")
 	// FileExists 文件已存在
-	FileExists = xcode.New(03205, "文件已存在")
+	FileExists = xcode.New(3205, "文件已存在")
 	// TestcaseExists 测试用例已存在
-	TestcaseExists = xcode.New(03206, "测试用例已存在")
+	TestcaseExists = xcode.New(3206, "测试用例已存在")
 )
